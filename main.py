2d57e8d931f9c0991c15d2a28b66bc0bb4a952f9
mattkram <mkramer@anaconda.com>
2025-03-24 10:42:59 -0500

    feat: Update to have a switch and styled lightbulb

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 from pyscript import Event
 from pyscript.web import button, div, page, when
 
-button_was_clicked = Event()
-times_clicked = 0
-light_is_on = False
+
+class Switch:
+    def __init__(self):
+        self.is_open = True
+
+        self.state_changed = Event()
+        self._id = f"switch-{hash(self)}"
+        self._element = button(id=self._id)
+        self.on_state_changed()
+
+    def on_click(self, e=None):
+        self.is_open = not self.is_open
+        self.state_changed.trigger(None)
+
+    def on_state_changed(self, e=None):
+        state = "open" if self.is_open else "closed"
+        self._element.innerHTML = f"The switch is {state}"
+
+    def finish(self):
+        # Late bind the click event handler
+        when("click", f"#{self._id}")(self.on_click)
+        when(self.state_changed)(self.on_state_changed)
+
+
+class Light:
+    def __init__(self):
+        self.is_on = False
+        self._element = div(className="lightbulb", id="my-div")
+
+    def light(self, e=None):
+        # Toggle the light state
+        self.is_on = not self.is_on
+        self._element.classList.toggle("on")
+
+    def finish(self):
+        when(switch.state_changed)(self.light)
+
+
+# Compose the UI
+light = Light()
+switch = Switch()
 
 page.append(
-    button(f"Clicked {times_clicked} times", id="my-button"),
-    div("The light is off!", id="my-div"),
+    switch._element,
+    light._element,
 )
 
-
-@when("click", "#my-button")
-def switch(e):
-    global times_clicked
-
-    times_clicked += 1
-    e.target.innerHTML = f"Clicked {times_clicked} times"
-    button_was_clicked.trigger(result="Hi")
-
-
-@when(button_was_clicked)
-def light(e):
-    global light_is_on
-
-    # Toggle the light state
-    light_is_on = not light_is_on
-
-    state = "on" if light_is_on else "off"
-    text = f"The light is {state}!"
-    page["#my-div"].innerHTML = text
+switch.finish()
+light.finish()
